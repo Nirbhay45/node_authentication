d697c30f250e9d8a5080ec826d5ba7acc2c0960b
Nirbhay45 <66776016+Nirbhay45@users.noreply.github.com>
2023-12-26 17:16:20 +0530

    Update constants.js

--- constants/constants.js
+++ constants/constants.js
@@ -3,6 +3,6 @@
 module.exports.app_name = "node-auth";
 module.exports.app_secret = "secretcode;)";
 module.exports.app_cookie_maxage = 1000*60*100;
-module.exports.mongo_url = "mongodb+srv://nirbhay:lHwjMnP1tJ43n6sl@cluster0.v9oagcn.mongodb.net/?retryWrites=true&w=majority";
+module.exports.mongo_url = "use your url 😉";
 module.exports.server_port = 8000;
 
